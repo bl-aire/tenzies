aad826372df8f862cb78af06aa35baebad71834e
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-04-10 20:52:55 +0100

    Create die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Content from './components/Content'
 import './App.css'
+import Die from './components/Die'
 
 function App() {
   return (
     <main>
       <Content />
+      <Die 
+        value="0"
+      />
     </main>
   )
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(prop){
+    return(
+        <main>
+
+        </main>
+    )
+}
