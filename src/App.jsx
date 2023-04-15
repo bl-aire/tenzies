eac6f5d003638aa20fd2adeb01084dc178974d29
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-04-15 19:04:09 +0100

    Change syntax of allNewDice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import Content from './components/Content'
 import './App.css'
 import Die from './components/Die'
+import React from 'react';
+
+
 
 function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
 
   function allNewDice(){
-    const a = Array.from(
-      {
-        length:6
-      }, () => (Math.random() * 6)
-    );
-    return a;
+    const newDice = []
+        for (let i = 0; i < 6; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
   }
+
+  const diceElements = dice.map(die => <Die value={die} />)
 
   return (
     <div className='main die_container'>
       <Content />
-        <Die value="4" />
-        <Die value="2" />
-        <Die value="4" />
-        <Die value="2" />
-        <Die value="4" />
-        <Die value="2" />
+      {diceElements}
     </div>
   )
 }
