f40e224da58ddce11851b76a13a497930756d821
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-04-15 18:02:11 +0100

    Create allNewDice function which returns array of random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import './App.css'
 import Die from './components/Die'
 
 function App() {
+
+  function allNewDice(){
+    const a = Array.from(
+      {
+        length:6
+      }, () => (Math.random() * 6)
+    );
+
+    console.log(a);
+  }
+
   return (
     <div className='main die_container'>
       <Content />
-      
         <Die value="4" />
         <Die value="2" />
         <Die value="4" />
         <Die value="2" />
         <Die value="4" />
         <Die value="2" />
-        <Die value="4" />
-        <Die value="2" />
-      
     </div>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 export default function Die(props){
     return(
         <div className="die_container">
-            <div className="die">{props.value}</div>
+            <h3 className="die">{props.value}</h3>
         </div>
     )
 }
