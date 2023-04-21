8912e8faeed588bb1f7e9679eae3a3fdf39fa064
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-04-21 23:44:07 +0100

    Add notification to tell when a player wins

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,13 @@ function App() {
   const [tenzies, setTenzies] = React.useState(false)
 
   React.useEffect(() => {
-    console.log("Dice state changed")
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSame = dice.every(die => die.value === firstValue)
+    if (allHeld && allSame) {
+      setTenzies(true)
+      console.log("You won")
+    }
   }, [dice])
 
   function generateNewDie() {
